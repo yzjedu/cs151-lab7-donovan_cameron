0953fe7adcc89ef214b3f95cbd02ef4c3676f261
cameron <cjcombariza@loyola.edu>
2024-10-27 20:45:50 -0400

    comments for the main.py

--- main.py
+++ main.py
@@ -8,18 +8,18 @@
 # Credits: basic area formula
 # Display the purpose of the program
 
-# Purpose:  [what problem does the function solve?]
-# Parameters: [list with purpose in the same order they appear in the function header]
-# Return: [return value, it's type, and what it represents]
+# Purpose:  enter floor input
+# Parameters: none
+# Return: floor_type
 def floor_input():
-    #
+    # input for floor type and error checking
     floor_type = input("Please enter the type of floor you would like to use: ")
     floor_type = floor_type.lower().strip()
     while floor_type != "carpet" and floor_type != "hardwood" and floor_type != "tile":
         floor_type = input("Please enter the type of floor you would like to use: ")
         floor_type = floor_type.lower().strip()
 
-    #
+    #type of floor and price
     if floor_type == "carpet":
         floor_type = float(1.39)
     elif floor_type == "hardwood":
@@ -28,9 +28,9 @@ def floor_input():
         floor_type = float(4.99)
     return floor_type
 
-# Purpose:  [what problem does the function solve?]
-# Parameters: [list with purpose in the same order they appear in the function header]
-# Return: [return value, it's type, and what it represents]
+# Purpose:  finds the area of the room
+# Parameters: none
+# Return: area
 def dimension_input():
     #
     length = input("Please enter the length of your room in feet to the nearest foot: ")
@@ -48,18 +48,18 @@ def dimension_input():
     area = length * width
     return area
 
-# Purpose:  [what problem does the function solve?]
-# Parameters: [list with purpose in the same order they appear in the function header]
-# Return: [return value, it's type, and what it represents]
+# Purpose:  cost of the room
+# Parameters: none
+# Return: room cost
 def room_cost():
     room_area = dimension_input()
     floor_material = floor_input()
     room_cost = floor_material * room_area
     return room_cost
 
-# Purpose:  [what problem does the function solve?]
-# Parameters: [list with purpose in the same order they appear in the function header]
-# Return: [return value, it's type, and what it represents]
+# Purpose:  main function, calls other functions
+# Parameters: none
+# Return: none
 def main():
     #
     print('In this program you can calculate the cost it would be to pay for each room and also all of them in total\n'
